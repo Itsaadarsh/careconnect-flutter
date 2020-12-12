0ea0cf2940e08ac53c61cf72c070e06a2d286480
Aadarsh <aadarsh-s@outlook.com>
2020-12-12 13:08:05 +0530

    Adding routes folders

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,3 +6,90 @@ import mongoose from 'mongoose';
 import socketio, { Socket } from 'socket.io';
 import vitalModel from './models/vitals';
 import path from 'path';
+
+const app = express();
+const httpserver = require('http').createServer(app);
+
+const PORT = process.env.PORT || 8080;
+
+// Adding user globally to express.Request
+declare global {
+  namespace Express {
+    interface Request {
+      user: string | object;
+    }
+  }
+}
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// CORS Handling
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method == 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,DELETE,PATCH');
+    return res.status(200).json({});
+  }
+  next();
+  return;
+});
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('/', (_, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+
+  app.get('/signup', (_, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+
+  app.get('/login', (_, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+}
+
+const io = socketio(httpserver);
+
+const userSocket = io.of('/');
+userSocket.on('connection', (socket: Socket) => {
+  console.log('Connected');
+  sendData(socket);
+});
+
+const sendData = async (socket: Socket) => {
+  const rand = (await Math.floor(Math.random() * (1900 - 10))) + 10;
+  const vitals = await vitalModel.find().skip(rand).limit(10);
+  socket.emit('data', vitals);
+  setTimeout(() => {
+    sendData(socket);
+  }, 10000);
+};
+
+// User Route
+app.use(userRouter);
+
+// Error handling
+app.use((_req, res, _next) => {
+  const err: Error = new Error('Invalid route');
+  res.json({
+    error: {
+      message: err.message,
+    },
+  });
+});
+
+// Listening to PORT and Connecting to Data Base
+httpserver.listen(PORT, async () => {
+  await mongoose.connect(
+    process.env.MONGO_URI ||
+      `mongodb+srv://aadi:${process.env.MONGO_PWD}@cluster0.b7dxw.mongodb.net/careconnect?retryWrites=true&w=majority`,
+    { useUnifiedTopology: true, useNewUrlParser: true }
+  );
+  console.log('Connected to Database');
+  console.log(`Listening at PORT ${PORT}`);
+});
---
+++ server/src/routes/user.ts
@@ -0,0 +1,130 @@
+import mongoose from 'mongoose';
+import express from 'express';
+import userModel from '../models/users';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import auth from '../auth/auth';
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+  const user = await userModel.find({ email: req.body.signupEmail });
+  if (user.length == 0) {
+    bcrypt.hash(req.body.signupPassword, 10, async (err, hash) => {
+      try {
+        if (!err) {
+          const user = new userModel({
+            _id: new mongoose.Types.ObjectId(),
+            name: req.body.name,
+            age: req.body.age,
+            weight: req.body.weight,
+            height: req.body.height,
+            phoneNum: req.body.phoneNum,
+            email: req.body.signupEmail,
+            password: hash,
+          });
+          const createdUser = await user.save();
+          const token: string = await jwt.sign(
+            { email: createdUser.email, userid: createdUser._id },
+            process.env.JWT_TOKEN!,
+            {
+              expiresIn: '24h',
+            }
+          );
+          res.status(201).json({
+            error: null,
+            data: {
+              userData: {
+                _id: createdUser._id,
+                name: createdUser.name,
+                age: createdUser.age,
+                weight: createdUser.weight,
+                height: createdUser.height,
+                phoneNum: createdUser.phoneNum,
+                email: createdUser.email,
+              },
+              token: token,
+            },
+          });
+        } else {
+          res.status(500).json({ error: 'Something went wrong.', data: null });
+        }
+      } catch (err) {
+        res.status(500).json({ error: err.message, data: null });
+      }
+    });
+  } else {
+    res.status(404).json({ error: 'Email already exists.', data: null });
+  }
+});
+
+router.get('/user', auth, async (req, res) => {
+  const user = await userModel.findOne({ email: req.user.email });
+  if (user === null) res.status(404).json({ error: 'User not found.', data: null });
+  else {
+    res.status(201).json({
+      error: null,
+      data: {
+        userData: {
+          _id: user._id,
+          name: user.name,
+          age: user.age,
+          weight: user.weight,
+          height: user.height,
+          phoneNum: user.phoneNum,
+          email: user.email,
+        },
+      },
+    });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const user = await userModel.findOne({ email: req.body.loginEmail });
+    if (user === null) res.status(404).json({ error: `User not found.`, data: null });
+    else {
+      bcrypt.compare(req.body.loginPassword, user.password, (err, pass) => {
+        if (err || pass == false) {
+          res.status(404).json({ error: 'Incorrect password.', data: null });
+        } else {
+          const token: string = jwt.sign({ email: user.email, userid: user._id }, process.env.JWT_TOKEN!, {
+            expiresIn: '72h',
+          });
+          res.status(201).json({
+            error: null,
+            data: {
+              userData: {
+                _id: user._id,
+                name: user.name,
+                age: user.age,
+                weight: user.weight,
+                height: user.height,
+                phoneNum: user.phoneNum,
+                email: user.email,
+              },
+              token: token,
+            },
+          });
+        }
+      });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message, data: null });
+  }
+});
+
+router.patch('/editprofile/:userid', auth, async (req, res) => {
+  try {
+    const updatedUser: any = {};
+    for (let item of req.body) {
+      updatedUser[item.method] = item.value;
+    }
+    const updatedValue = await userModel.updateOne({ _id: req.params.userid }, { $set: updatedUser });
+    if (updatedValue.n === 1) res.status(201).json({ error: null, data: { userData: updatedValue } });
+    else res.status(500).json({ error: 'Something went wrong.', data: null });
+  } catch (err) {
+    res.status(500).json({ error: err.message, data: null });
+  }
+});
+
+export default module.exports = router;
